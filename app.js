ec7c93f2dd240ad353509f9c7182292af5454ac8
jstm17 <62430328+jstm17@users.noreply.github.com>
2023-11-17 16:55:40 +0100

    add footer + fix

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ const cards = document.querySelectorAll(".card");
 
 cards.forEach(card => {
     card.addEventListener("mouseover", function () {
-        this.querySelector("#card-front").classList.add('hidden');
-        this.querySelector("#card-back").classList.remove('hidden')
+        this.querySelector("#card-front").classList.add('opacity');
+        this.querySelector("#card-back").classList.remove('opacity')
     })
 
     card.addEventListener("mouseout", function () {
-        this.querySelector("#card-front").classList.remove('hidden');
-        this.querySelector("#card-back").classList.add('hidden')
+        this.querySelector("#card-front").classList.remove('opacity');
+        this.querySelector("#card-back").classList.add('opacity')
     })
 });
 
@@ -36,3 +36,15 @@ window.addEventListener('scroll', function () {
       header.classList.remove('scrolled');
     }
   });
+
+
+  const menuTrigger = document.querySelector('[data-ic-class="button-trigger"]');
+    const menuOverlay = document.querySelector('[data-ic-class="overlay"]');
+    const menuItem = document.querySelector('.menu-item');
+    const activeClass = 'active';
+
+menuTrigger.addEventListener("click", function(){
+  menuTrigger.classList.toggle(activeClass);
+  document.querySelector('.burger-menu').classList.toggle('open');
+  document.querySelector('header').classList.toggle('open');
+});
